7a4a243a294201cf153911859a4c9666e53d6fa2
SDM-BQA <sdmbqa420@gmail.com>
2023-02-23 23:53:27 +0530

    about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4 text-justify'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint atque laborum et natus adipisci neque sequi cum exercitationem velit vitae maiores impedit, perspiciatis optio amet harum voluptas sit suscipit. Expedita fuga sed repudiandae repellendus sunt labore quod placeat impedit iste, totam, velit dolorem suscipit similique nihil quasi ab a! Qui illo impedit soluta repellat quo porro fugit eum laboriosam facilis illum assumenda vitae reprehenderit quisquam commodi velit doloribus dolorum, adipisci quos omnis neque dolores magni minima, voluptates sed! Ut mollitia ipsum exercitationem dolor molestias temporibus suscipit! Doloremque quidem molestias alias possimus ex non consectetur, quam dignissimos, a aperiam laboriosam ducimus at! Dolor perspiciatis, dolorum quasi rem omnis eligendi esse aspernatur harum est commodi
+      </p>
+      <p> qui eum nihil nesciunt a suscipit fugiat repellat voluptas voluptates quae autem distinctio aliquid. Quia accusamus laboriosam adipisci doloremque harum dolorum corrupti atque suscipit repellendus aperiam? Repellendus inventore porro nemo sequi, laudantium dolorum perferendis nam quod blanditiis vitae in est quas deleniti beatae eligendi excepturi tenetur quo fuga optio. Alias a error, tempora impedit ipsum dignissimos numquam quos necessitatibus cupiditate odit, eligimos. Qui, optio harum, quos amet obcaecati ab vitae iste odit nam aspernatur veniam dolor aliquid impedit possimus doloremque magnam magni ratione eligendi. Ex autem quam vitae non enetur optio incidunt a, sed illo veniam similique dignissimos reprehenderit dolorum laboriosam, ab praesentium eos tempora ipsum mollitia consequuntur rerum dolores aspernatur omnis. Est, voluptas necessitatibus mollitspernatur alias laboriosam, a ipsa magnam eos exercitationem quis ut ea necessitatibus repellendus? Laudantium tenetur quasi quas veniam modi id dolor.</p>
+    </div>
+  )
+}
