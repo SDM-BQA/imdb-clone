f25e71915a24a97149ca619a9b37f0bae6e24982
SDM-BQA <sdmbqa420@gmail.com>
2023-02-23 23:42:06 +0530

    working on darkmode using next-theme

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,6 +3,7 @@ import Menuitem from './Menuitem'
 import {AiFillHome} from 'react-icons/ai'
 import {AiFillExclamationCircle} from 'react-icons/ai'
 import Link from 'next/link'
+import DarkModeSwitch from './DarkModeSwitch'
 
 
 export default function Header() {
@@ -12,7 +13,8 @@ export default function Header() {
         <Menuitem title={"Home"} address="/" Icon={AiFillHome}/>
         <Menuitem title={"About"} address="/about" Icon={AiFillExclamationCircle}/>
       </div>
-      <div className='mx-4'>
+      <div className='mx-4 flex items-center space-x-5'>
+        <DarkModeSwitch/>
         <Link href="/">
             <h2 className='text-2xl'><span className='font-bold bg-amber-500 py-1 px-1 rounded-lg mr-1'>IMDB</span><span className='text-xl hidden sm:inline'>Clone</span></h2>
         </Link>
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,11 +2,13 @@ import Header from './components/Header'
 import Navbar from './components/Navbar'
 import Search from './components/Search'
 import './globals.css'
+import Providers from './Providers'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Providers>
         {/* header */}
       <Header/>
         {/* navbar */}
@@ -14,6 +16,7 @@ export default function RootLayout({ children }) {
         {/* search */}
       <Search/>
         {children}
+        </Providers>
         </body>
     </html>
   )
